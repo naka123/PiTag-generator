e50da526ada1cb9152fe0900822a628d69e35303
naka123 <naka123@kukabarra.info>
2020-11-14 18:35:18 +0600

    width/height, py3

--- pi-tag_gen.py
+++ pi-tag_gen.py
@@ -21,7 +21,8 @@ def main(argv):
     parser.add_argument("AC0", help="Cross relation AC in line 0",default=0.60, type=float)
     parser.add_argument("AB1", help="Cross relation AB in line 1",default=0.20, type=float)
     parser.add_argument("AC1", help="Cross relation AC in line 1",default=0.80, type=float)
-    parser.add_argument("--marker_size", help="Length from center of top-left circle to center of top-right circle (cm)", default=10.0, type=float)
+    parser.add_argument("--marker_width", help="Length from center of top-left circle to center of top-right circle (cm)", default=15.0, type=float)
+    parser.add_argument("--marker_height", help="Length from center of top-left circle to center of bottom-left circle (cm)", default=10.0, type=float)
     parser.add_argument("--circle_clearance", help="White space around each black circle in cm", default=0.2, type=float)
     parser.add_argument("--circle_radius", help="Radius of each circle (cm)", default=0.9, type=float)
     parser.add_argument("--output_file", help="Name of the output file", type=str, default="pitag_marker.svg")
@@ -47,30 +48,31 @@ def main(argv):
     
 
     #The four corners of the square
-    marker_size = args.marker_size
+    marker_width = args.marker_width
+    marker_height = args.marker_height
     circle_radius = args.circle_radius
     circle_clearance = args.circle_clearance  
     
     #CrossRatios 0 - 1.0 relative to marker_size
     
-    d_line0_AB = marker_size * args.AB0 #AB
-    d_line0_BD = marker_size - marker_size *args.AB0 #BD
-    d_line0_AC = marker_size * args.AC0 #//AC
-    d_line0_CD = marker_size - marker_size * args.AC0 #//CD
+    d_line0_AB = args.AB0 #AB
+    d_line0_BD = 1 - args.AB0 #BD
+    d_line0_AC = args.AC0 #//AC
+    d_line0_CD = 1 - args.AC0 #//CD
     cross_ration_0 = (d_line0_AB/d_line0_BD)/(d_line0_AC/d_line0_CD)
     
-    d_line1_AB = marker_size * args.AB1 #AB
-    d_line1_BD = marker_size - marker_size *args.AB1 #BD
-    d_line1_AC = marker_size * args.AC1 #;//AC
-    d_line1_CD = marker_size - marker_size * args.AC1 #;//CD
+    d_line1_AB = args.AB1 #AB
+    d_line1_BD = 1 - args.AB1 #BD
+    d_line1_AC = args.AC1 #;//AC
+    d_line1_CD = 1 - args.AC1 #;//CD
     cross_ration_1 = (d_line1_AB/d_line1_BD)/(d_line1_AC/d_line1_CD)
-    
-    print "cross_ration 0:  {0}".format(cross_ration_0)    
-    print "cross_ration 1:  {0}".format(cross_ration_1)    
-    print "delta:{0}".format(cross_ration_0 / cross_ration_1)    
-    
+
+    print("cross_ration 0:  {0}".format(cross_ration_0))
+    print("cross_ration 1:  {0}".format(cross_ration_1))
+    print("delta:{0}".format(cross_ration_0 / cross_ration_1))
+
     if cross_ration_0 < cross_ration_1:
-        print "Error, Crossratio 0 must be greater that Crossratio 1"
+        print("Error, Crossratio 0 must be greater that Crossratio 1")
         sys.exit()
     
     CR_Line0_AB = args.AB0
@@ -79,30 +81,36 @@ def main(argv):
     CR_Line1_AC = args.AC1
     
     #center of the marker
-    cx = cy = circle_clearance + circle_radius + marker_size/2
+    #cx = cy = circle_clearance + circle_radius + marker_size/2
+    cx = circle_clearance + circle_radius + marker_width / 2
+    cy = circle_clearance + circle_radius + marker_height / 2
     
     
     
     #Squares for marker size reference
-    size_inner_square = marker_size-circle_radius*2-circle_clearance*2
+    size_inner_rect_width = marker_width - circle_radius * 2 - circle_clearance * 2
+    size_inner_rect_height = marker_height - circle_radius * 2 - circle_clearance * 2
     corner_inner_square = 2*circle_radius + 2*circle_clearance
-    size_outer_square = marker_size+circle_radius*2+circle_clearance*2
+    size_outer_rect_width = marker_width + circle_radius * 2 + circle_clearance * 2
+    size_outer_rect_height = marker_height + circle_radius * 2 + circle_clearance * 2
     
     #We create a group for the whole marker
     
     marker = dwg.defs.add(dwg.g(id='marker'))
     
     #Outer square
-    marker.add(svgwrite.shapes.Rect((0*cm,0*cm),(size_outer_square*cm,size_outer_square*cm), fill='white', stroke='black'))
+    marker.add(svgwrite.shapes.Rect((0*cm,0*cm),(size_outer_rect_width*cm,size_outer_rect_height*cm), fill='white', stroke='black'))
     
     #inner square
-    marker.add(svgwrite.shapes.Rect((corner_inner_square*cm,corner_inner_square*cm),(size_inner_square*cm,size_inner_square*cm), fill='none', stroke='black'))
+    marker.add(svgwrite.shapes.Rect((corner_inner_square*cm,corner_inner_square*cm),(size_inner_rect_width*cm,size_inner_rect_height*cm), fill='none', stroke='black'))
     
     
     
     
-    x1 = y1 = circle_radius + circle_clearance
-    x2 = y2 = circle_radius + circle_clearance + marker_size
+    x1 = circle_radius + circle_clearance
+    y1 = circle_radius + circle_clearance
+    x2 = circle_radius + circle_clearance + marker_width
+    y2 = circle_radius + circle_clearance + marker_height
     
     #lets draw the marker corner circles
     top_left_corner = (x1*cm,y1*cm)
@@ -121,23 +129,23 @@ def main(argv):
     
     
     
-    marker.add(svgwrite.shapes.Circle(((x1+marker_size*CR_Line0_AB)*cm,y1*cm), circle_radius*cm))
-    marker.add(svgwrite.shapes.Circle((x1*cm,(y1+marker_size*CR_Line0_AB)*cm), circle_radius*cm))
-    
-    marker.add(svgwrite.shapes.Circle(((x1+marker_size*CR_Line0_AC)*cm,y1*cm), circle_radius*cm))
-    marker.add(svgwrite.shapes.Circle((x1*cm,(y1+marker_size*CR_Line0_AC)*cm), circle_radius*cm))
+    marker.add(svgwrite.shapes.Circle(((x1+marker_width*CR_Line0_AB)*cm,y1*cm), circle_radius*cm))
+    marker.add(svgwrite.shapes.Circle(((x1+marker_width*CR_Line0_AC)*cm,y1*cm), circle_radius*cm))
+
+    marker.add(svgwrite.shapes.Circle((x1*cm,(y1+marker_height*CR_Line0_AB)*cm), circle_radius*cm))
+    marker.add(svgwrite.shapes.Circle((x1*cm,(y1+marker_height*CR_Line0_AC)*cm), circle_radius*cm))
     
     #Now we draw the Cross related circles for line 1
-    marker.add(svgwrite.shapes.Circle(((x1+marker_size*CR_Line1_AB)*cm,y2*cm), circle_radius*cm))
-    marker.add(svgwrite.shapes.Circle((x2*cm,(y1+marker_size*CR_Line1_AB)*cm), circle_radius*cm))
-    
-    marker.add(svgwrite.shapes.Circle(((x1+marker_size*CR_Line1_AC)*cm,y2*cm), circle_radius*cm))
-    marker.add(svgwrite.shapes.Circle((x2*cm,(y1+marker_size*CR_Line1_AC)*cm), circle_radius*cm))
+    marker.add(svgwrite.shapes.Circle(((x1+marker_width*CR_Line1_AB)*cm,y2*cm), circle_radius*cm))
+    marker.add(svgwrite.shapes.Circle(((x1+marker_width*CR_Line1_AC)*cm,y2*cm), circle_radius*cm))
+
+    marker.add(svgwrite.shapes.Circle((x2*cm,(y1+marker_height*CR_Line1_AB)*cm), circle_radius*cm))
+    marker.add(svgwrite.shapes.Circle((x2*cm,(y1+marker_height*CR_Line1_AC)*cm), circle_radius*cm))
     
     
     #Put text with information about the marker in the center
     t_x = cx - 5
-    t_y = cy + size_outer_square/2 +1
+    t_y = cy + size_outer_rect_height/2 +1
     
     if args.show_info:
         marker.add(dwg.text('| AB | BD | CD |', insert=((t_x+3)*cm, t_y*cm), fill='black', textLength = 10*cm))
@@ -151,21 +159,8 @@ def main(argv):
         marker.add(dwg.text('Circle Clearance = {0}cm'.format(circle_clearance), insert=((t_x)*cm, (t_y+2+1.0)*cm), fill='black'))
         marker.add(dwg.text('Cross-ratio 0: AB = {0:2.2f},  AC= {1:.2f}, CR= {2:.4f}'.format(CR_Line0_AB,CR_Line0_AC, cross_ration_0,4), insert=((t_x)*cm, (t_y+2+1.5)*cm), fill='black'))
         marker.add(dwg.text('Cross-ratio 1: AB = {0:2.2f},  AC= {1:.2f}, CR= {2:.4f}'.format(CR_Line1_AB,CR_Line1_AC, cross_ration_1,4), insert=((t_x)*cm, (t_y+2+2)*cm), fill='black'))
-        
-        
-    
-    
-    d_line0_AB = marker_size * args.AB0 #AB
-    d_line0_BD = marker_size - marker_size *args.AB0 #BD
-    d_line0_AC = marker_size * args.AC0 #//AC
-    d_line0_CD = marker_size - marker_size * args.AC0 #//CD
-    cross_ration_0 = (d_line0_AB/d_line0_BD)/(d_line0_AC/d_line0_CD)
-    
-    d_line1_AB = marker_size * args.AB1 #AB
-    d_line1_BD = marker_size - marker_size *args.AB1 #BD
-    d_line1_AC = marker_size * args.AC1 #;//AC
-    d_line1_CD = marker_size - marker_size * args.AC1 #;//CD
-    
+
+
     #red do in the Center of the marker
     marker.add(svgwrite.shapes.Circle((cx*cm,cy*cm), 0.05*cm, fill='red'))
     
@@ -177,8 +172,8 @@ def main(argv):
         
     if args.A4:
         #center marker in middle of A4 sheet
-        x_m = ((w/2)-(size_outer_square/2))
-        y_m = ((h/2)-(size_outer_square/2))
+        x_m = ((w/2)-(size_outer_rect_width/2))
+        y_m = ((h/2)-(size_outer_rect_height/2))
         u = dwg.use(marker, insert=(x_m*cm,y_m*cm))
     else:
         u = dwg.use(marker, insert=(0*cm,0*cm))
